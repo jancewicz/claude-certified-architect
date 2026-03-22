87ffa8472b128376a1cb17a2e3f33ad4617ec629
jancewicz <jancewicz.pawel1@gmail.com>
2026-03-22 13:10:48 +0100

    Another util function for text chunking

--- c1_building_with_claude_api/retrieval_augmented_generation/chunk_text.py
+++ c1_building_with_claude_api/retrieval_augmented_generation/chunk_text.py
@@ -1,3 +1,6 @@
+import re
+
+
 def chunk_by_char(text, chunk_size: int = 150, chunk_overlap: int = 20):
     chunks: list[str] = []
     start_idx: int = 0
@@ -10,3 +13,27 @@ def chunk_by_char(text, chunk_size: int = 150, chunk_overlap: int = 20):
 
         start_idx = end_idx - chunk_overlap if end_idx < len(text) else len(text)
     return chunks
+
+
+def chunk_by_sentence(text, max_sentences_per_chunk=5, overlap_sentences=1):
+    sentences = re.split(r"(?<=[.!?])\s+", text)
+
+    chunks = []
+    start_idx = 0
+
+    while start_idx < len(sentences):
+        end_idx = min(start_idx + max_sentences_per_chunk, len(sentences))
+
+        current_chunk = sentences[start_idx:end_idx]
+        chunks.append(" ".join(current_chunk))
+        start_idx += max_sentences_per_chunk - overlap_sentences
+
+        if start_idx < 0:
+            start_idx = 0
+    return chunks
+
+
+def chunk_by_section(document_text):
+    # Split doc by Markdown headers - fragile as we depend on certain structure of a document and its formatting
+    pattern = r"\n## "
+    return re.split(pattern, document_text)
